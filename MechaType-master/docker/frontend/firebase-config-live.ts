e04830708a7a9e7a410e56cf6953bb9c9bc3ed57
Manrique Antonio <manrique1234tt@gmail.com>
2025-02-11 07:29:00 -0600

    se comento para explicar su funcionamiento

--- MechaType-master/docker/frontend/firebase-config-live.ts
+++ MechaType-master/docker/frontend/firebase-config-live.ts
@@ -1,13 +1,26 @@
-// To find your config, go to https://console.firebase.google.com/ and select your project
-// Go to (top left) Settings > Project Settings > General
-// scroll down to Your apps > Web Apps (if it doesnt exist, create one) > SDK setup and configuration > select npm
-// your config should be visible there
+// Para encontrar tu configuración de Firebase, sigue estos pasos:
+// 1. Ve a https://console.firebase.google.com/ y selecciona tu proyecto.
+// 2. En la parte superior izquierda, haz clic en "Settings" > "Project Settings" > "General".
+// 3. Desplázate hacia abajo hasta "Your apps" > "Web Apps" (si no existe, crea una).
+// 4. Selecciona "SDK setup and configuration" > "npm".
+// 5. Tu configuración estará visible allí.
 
 export const firebaseConfig = {
+  // Clave API de Firebase para autenticar las solicitudes.
   apiKey: "###FIREBASE_APIKEY###",
+
+  // Dominio de autenticación de Firebase.
   authDomain: "###FIREBASE_AUTHDOMAIN###",
+
+  // ID del proyecto en Firebase.
   projectId: "###FIREBASE_PROJECTID###",
+
+  // Bucket de almacenamiento de Firebase para archivos.
   storageBucket: "###FIREBASE_STORAGEBUCKET###",
+
+  // ID del remitente para mensajería (usado en Firebase Cloud Messaging).
   messagingSenderId: "###FIREBASE_MESSAGINGSENDERID###",
+
+  // ID de la aplicación Firebase.
   appId: "###FIREBASE_APPID###",
 };
